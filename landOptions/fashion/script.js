0ad333da0aba25ccb273aeebccac782626197391
Karanja-max <michaelnganga4787@gmail.com>
2024-11-27 10:27:13 +0300

    Update script.js

--- landOptions/fashion/script.js
+++ landOptions/fashion/script.js
@@ -11,7 +11,7 @@ const questions = [
   {
     question: "What is the name of the style of clothing that originated in the 1950s and is characterized by its tight-fitting silhouette and emphasis on the hips?",
     answers: [
-      { text: "New look", correct: true },
+      { text: "Hippie", correct: false },
       { text: "Hippie", correct: false },
       { text: "Flapper", correct: false },
       { text: "Mod", correct: false },
